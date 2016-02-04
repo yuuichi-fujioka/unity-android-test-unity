4c4534b36cf7c4f7c3dcb111176a11ef63c783eb
yuuichi fujioka <fujioka.yuuichi@gmail.com>
2016-02-04 21:11:19 +0900

    Calling Java

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -13,10 +13,14 @@ namespace Hoge
         private long delta3 = -1;
         private long delta4 = -1;
         private long delta5 = -1;
+        private long delta6 = -1;
+        private long delta7 = -1;
         private int v;
         private int v2;
         private int v3;
         private int v4;
+        private int v5;
+        private int v6;
         private static bool called;
         [DllImport("hello-jni")]
         private static extern int HelloWorld();
@@ -28,6 +32,8 @@ namespace Hoge
         [DllImport("hello-jni")]
         private static extern int HelloWorld4(Callback callback);
 
+        private AndroidJavaClass hoge;
+
         public static int Hello(int v)
         {
             called = true;
@@ -37,7 +43,7 @@ namespace Hoge
         // Use this for initialization
         void Start()
         {
-
+            hoge = new AndroidJavaClass("jp.test.lib.Hoge");
         }
 
         // Update is called once per frame
@@ -63,6 +69,14 @@ namespace Hoge
             v4 = HelloWorld4(Hello);
             delta5 = DateTime.Now.Ticks - t;
 
+            t = DateTime.Now.Ticks;
+            v5 = hoge.CallStatic<int>("Hello");
+            delta6 = DateTime.Now.Ticks - t;
+
+            object[] param = new object[] { data };
+            t = DateTime.Now.Ticks;
+            v6 = hoge.CallStatic<int>("Hello2", param);
+            delta7 = DateTime.Now.Ticks - t;
         }
 
         void OnGUI()
@@ -77,7 +91,11 @@ namespace Hoge
             GUI.Label(new Rect(90, 70, 200, 200), called.ToString());
             GUI.Label(new Rect(30, 90, 200, 200), v4.ToString());
             GUI.Label(new Rect(60, 90, 200, 200), delta5.ToString());
-            GUI.Label(new Rect(30, 110, 200, 200), "V8");
+            GUI.Label(new Rect(30, 110, 200, 200), v5.ToString());
+            GUI.Label(new Rect(60, 110, 200, 200), delta6.ToString());
+            GUI.Label(new Rect(30, 130, 200, 200), v6.ToString());
+            GUI.Label(new Rect(60, 130, 200, 200), delta7.ToString());
+            GUI.Label(new Rect(30, 20, 200, 200), "V9");
         }
     }
 }
